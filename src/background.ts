fdc0b4a857c094aae50795d238debb5f898b1b7c
Li-Hsuan <lihsuanlung@gmail.com>
2020-08-22 02:28:54 -0400

    Create new alarms after the break alarm is triggered

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,5 @@
 import Beep from './beep';
+import Local from './local';
 
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {
@@ -6,6 +7,22 @@ chrome.runtime.onInstalled.addListener((details) => {
   }
 });
 
-chrome.alarms.onAlarm.addListener(() => {
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === 'break') {
+    const state = Local.load();
+    const now = new Date().getTime();
+    const workTime = now + (state.work * 60 * 1000);
+    const breakTime = workTime + (state.break * 60 * 1000);
+
+    chrome.alarms.create('work', { when: workTime });
+    chrome.alarms.create('break', { when: breakTime });
+
+    Local.save({
+      ...state,
+      start: now,
+      current: now,
+    });
+  }
+
   Beep.play(3);
 });
